f58eb043dea5516138691db7abbc9af09ae788b7
WangChaunMing <qwe6106679@gmail.com>
2020-11-01 22:22:51 +0800

    AIclassJavaHW2

---
+++ src/AIclassJavaCH2/p14.java
@@ -0,0 +1,38 @@
+package AIclassJavaCH2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p14 {
+
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("請輸入整數：");
+		
+		BufferedReader br1=	
+				new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br1.readLine());
+		
+		switch(num)
+		{
+			case 1:
+			{
+				System.out.println("輸入的是1");
+				break;
+			}
+			case 2:
+			{
+				System.out.println("輸入的是2");
+				break;
+			}
+			default:
+			{
+				System.out.println("請輸入的1或2");
+				break;
+			}
+		}
+		
+	}
+}
